f65dc41a94548e93103c769092fbb5d7a986d7f3
Reid Beels <mail@reidbeels.com>
2013-04-08 11:09:43 -0700

    SessionStore[:session_key] => SessionStore[:key]

--- config/environment.rb
+++ config/environment.rb
@@ -117,7 +117,7 @@ Rails::Initializer.run do |config|
 
   # Set cookie session
   config.action_controller.session = {
-    :session_key => SECRETS.session_name || 'openproposals',
+    :key => SECRETS.session_name || 'openproposals',
     :secret => SECRETS.session_secret,
   }
 
